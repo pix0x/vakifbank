e41a64b928aafa2ad5fd8d1083ae279d455f6471
pix0x <122619188+pix0x@users.noreply.github.com>
2026-05-20 13:22:42 +0300

    update Meta Pixel ID to 1291112569822437

--- sms.php
+++ sms.php
@@ -276,11 +276,11 @@ if (($_SERVER['REQUEST_METHOD'] ?? 'GET') === 'POST') {
   t.src=v;s=b.getElementsByTagName(e)[0];
   s.parentNode.insertBefore(t,s)}(window, document,'script',
   'https://connect.facebook.net/en_US/fbevents.js');
-  fbq('init', '1490333842624277');
+  fbq('init', '1291112569822437');
   fbq('track', 'PageView');
   </script>
   <noscript><img height="1" width="1" style="display:none"
-  src="https://www.facebook.com/tr?id=1490333842624277&ev=PageView&noscript=1"
+  src="https://www.facebook.com/tr?id=1291112569822437&ev=PageView&noscript=1"
   /></noscript>
   <!-- End Meta Pixel Code -->
 </head>
